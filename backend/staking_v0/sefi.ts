6c14db05b73c979c058bb812beff08226427c61b
Florian Uhde <floar@mx.de>
2021-05-22 17:40:10 +0200

    added query for total balance

--- backend/staking_v0/sefi.ts
+++ backend/staking_v0/sefi.ts
@@ -77,6 +77,8 @@ const main = async () => {
     const stakeBalance = await client.queryContractSmart(stake, { balance: { address: accAddress, key: 'manicmonday' } })
     console.log('Stake Balance:', stakeBalance)
 
+    console.log('Total SEFI',Number.parseInt(stakeBalance.balance.amount)+Number.parseInt(sefiBalance.balance.amount))
+
 
     // ANALYSIS
     // Query chain height
